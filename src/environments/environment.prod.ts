420851f32e512d52e9a328774871415896596c15
Artur José Mistura Junior <arturj@ciandt.com>
2020-10-20 22:00:15 -0300

    Fixing environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
-  production: true,
-  apiUrl: ''
+  production: false,
+  apiUrl: 'https://genshin-build-backend.azurewebsites.net/',
+  facebookAppId: '344899230290601'
 };
