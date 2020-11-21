35d7acc639b3825ae2c7aeff7866a94be4c18bd8
Artur José Mistura Junior <arturj@ciandt.com>
2020-11-21 01:00:27 -0300

    staging environment

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://gb-backend-ussnrq3fsa-uc.a.run.app/',
+  apiUrl: 'https://gb-backend-develop-ussnrq3fsa-uc.a.run.app',
   facebookAppId: '344899230290601'
 };
